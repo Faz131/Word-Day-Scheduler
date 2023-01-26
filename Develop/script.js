c60485cb0ad0722e8ad44e9e38563fc14c71ccba
Faz131 <fazms@icloud.com>
2023-01-25 20:53:51 -0700

    added some code

--- Develop/script.js
+++ Develop/script.js
@@ -1,7 +1,20 @@
 // Wrap all code that interacts with the DOM in a call to jQuery to ensure that
 // the code isn't run until the browser has finished rendering all the elements
 // in the html.
+var saveBTN = $('.btn saveBtn');
+var hour9 = $('#hour-9');
+
+
 $(function () {
+  // var saveHour = function (event) {
+
+  //   event.preventDefault();
+  //   var saveBTN =
+
+  // }
+
+
+
   // TODO: Add a listener for click events on the save button. This code should
   // use the id in the containing time-block as a key to save the user input in
   // local storage. HINT: What does `this` reference in the click listener
@@ -20,4 +33,6 @@ $(function () {
   // attribute of each time-block be used to do this?
   //
   // TODO: Add code to display the current date in the header of the page.
+  var today = dayjs();
+  $('#currentDay').text(today.format('dddd, MMMM, D'));
 });
